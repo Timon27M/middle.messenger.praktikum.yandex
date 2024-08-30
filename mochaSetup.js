a83c820d5279172a3a99b759e9aa9f968a49e3c5
Timofey Pichugin <tpich87@gmail.com>
2024-08-30 18:22:09 +0300

    feat: new

--- mochaSetup.js
+++ mochaSetup.js
@@ -2,7 +2,7 @@ import { JSDOM } from "jsdom";
 
 const jsdom = new JSDOM("<body></body>", { url: "http://localhost" });
 
-global.window = jsdom.window;
+global.window = jsdom.window
 global.document = jsdom.window.document;
 global.Node = jsdom.window.Node;
 global.MouseEvent = jsdom.window.MouseEvent;
